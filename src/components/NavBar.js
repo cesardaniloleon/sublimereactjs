122660db667be6462299db5c1f44d4da6176e4ea
cesardaniloleon <cesardanilo059@gmail.com>
2022-04-18 20:51:29 -0300

    Added components

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1>Sublime</h1>
+        <li><a href='#'>Inicio</a></li>
+        <li><a href='#'>Sahumerios</a></li>
+        <li><a href='#'>Sales Aromaticas</a></li>
+        <li><a href='#'>Sahumos</a></li>
+    </div>
+  ) 
+}
+export default NavBar
